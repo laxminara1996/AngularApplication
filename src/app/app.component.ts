dd13ea9f6be5b7013abfc0a21f64f34989deaa3d
laxminara1996 <laxminara1996@gmail.com>
2023-08-29 20:02:35 +0530

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'myapp';
+  title:String = 'myapp';
+  message:String="welcome to the world";
+
+  method():void{
+    alert('inside the method');
+  }
 }
